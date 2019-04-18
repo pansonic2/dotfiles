9b42a6e8fd1c35c823b2ae7ae067b67345484c95
pansonic2 <thehoteinstein@gmail.com>
2019-04-18 12:04:45 +0500

    added cask config

---
+++ caskconfig.sh
@@ -0,0 +1,36 @@
+#!/bin/sh 
+brew cask install aegisub
+brew cask install android-file-transfer
+brew cask install android-studio
+brew install avidemux
+brew cask install caffeine
+brew cask install cyberduck
+brew cask install db-browser-for-sqlite
+brew cask install disk-inventory-x
+brew cask install firefox
+brew cask install google-chrome
+brew cask install handbrake
+brew cask install keka
+brew cask install locko
+brew cask install pycharm-ce
+brew cask install sdformatter
+brew cask install skype
+brew cask install sonarr
+brew cask install soulseek
+brew cask install spotify
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install vlc
+brew cask install visual-studio-code
+brew install wine
+brew cask install xnviewmp
+brew cask install balenaetcher
+brew cask install calibre
+brew cask install ifunbox
+brew cask install bitwarden
+brew install ffmpeg
+brew install youtube-dl
+
+
+
